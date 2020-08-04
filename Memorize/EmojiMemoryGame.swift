efb4eae3f4c436dbb4b5d3a86159b438f943ded8
Marlon Trujillo Ariza <marlon.trujillo2001@gmail.com>
2020-08-04 12:19:42 -0700

    Lecture 2

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Marlon Trujillo Ariza on 8/3/20.
+//  Copyright © 2020 Marlon Trujillo Ariza. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card){
+        model.choose(card: card)
+    }
+}
